5029ab4a372a0781e23d1a5329bc8e5371adeb98
DairanDoo <your_email@example.com>
2024-05-22 03:17:18 -0400

    Manejo de exepciones y relative imports.

---
+++ Curso_RestApi_With_Flask/31_errors_in_python/main.py
@@ -0,0 +1,23 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.") #ValueError, RuntimeError, TypeError... 
+    
+    return dividend / divisor 
+
+
+grades = []
+
+print("Welcome to the average grade program.")
+
+try:
+    average = divide(sum(grades), len(grades))
+    
+except ZeroDivisionError as e: # e takes the value of the error in the variable.
+
+    print("There are no grades yet in your list.")
+else: # if no error happens, the else executes. 
+    print(f"The average grade is {average}.")
+    
+finally: # the finally will execute always, if the try or catch, dosent matter.
+    print("Thank you!")
+    
