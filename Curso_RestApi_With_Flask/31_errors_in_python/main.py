63abb4a044f39c6f0917aec792a1981cc4033c1d
DairanDoo <your_email@example.com>
2024-05-24 02:32:15 -0400

    Más ejercicios, manejo de errores.

--- Curso_RestApi_With_Flask/31_errors_in_python/main.py
+++ Curso_RestApi_With_Flask/31_errors_in_python/main.py
@@ -13,9 +13,8 @@ try:
     average = divide(sum(grades), len(grades))
     
 except ZeroDivisionError as e: # e takes the value of the error in the variable.
-
     print("There are no grades yet in your list.")
-else: # if no error happens, the else executes. 
+else: # if no error happens, the else also executes. 
     print(f"The average grade is {average}.")
     
 finally: # the finally will execute always, if the try or catch, dosent matter.
