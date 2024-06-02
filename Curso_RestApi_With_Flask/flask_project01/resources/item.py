59858a45f01855500eb6e47d502d3da6a5b517f8
DairanDoo <your_email@example.com>
2024-06-01 21:09:11 -0400

    Sql Alchemy, Marshmallow, Modelos, Schemas, relaciones y usando requirements para actualizar las dependencias, más usando docker.

--- Curso_RestApi_With_Flask/flask_project01/resources/item.py
+++ Curso_RestApi_With_Flask/flask_project01/resources/item.py
@@ -1,9 +1,9 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
 
-from db import items
+from db import db
+from models import ItemModel
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", "items", description="Operations on items")
@@ -11,52 +11,48 @@ blp = Blueprint("Items", "items", description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
-    @blp.response(200, ItemSchema) # aquí va el status code y el itemSchema a la derecha recibe el valor devuelto en el return.
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
-
-    @blp.arguments(ItemUpdateSchema) # al usar el blp.arguments, el parametro va en frente del resto de los otros argumentos.
-    @blp.response(200, ItemSchema) # el orden importa, el response debe ir dentro del arguments, más abajo.
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        
+        return {"Message": "Item deleted."}
+    
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        try:
-            item = items[item_id]
-
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data
-
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get(item_id) # no se puede usar el get_or_404 por que si no se pasa un item_id, tampoco se creará uno.
+        if item: # si existe, se sustituyen los valores de item_data por los recibios en el put.
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else: # si no existe, se crea un nuevo item, usando el store_id que se pasa en él.
+            item = ItemModel(id = item_id, **item_data)
+            
+        db.session.add(item)
+        db.session.commit()
+        
+        return item
 
 
 @blp.route("/item")
 class ItemList(MethodView):
-    @blp.response(200, ItemSchema(many=True)) # el many=True se usa cuando se devuelve multiples itemSchemas.
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values() # ahora simplemente devolvemos el items.values()
+        return ItemModel.query.all()
 
-    @blp.arguments(ItemSchema) # aquí usamos el schema para validar 
+    @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
-    def post(self, item_data): # ahora no hay que hacer el request ya que el item schema lo recibe y valida (luego devolviendo como argumento ese diccionario validado.).
-        for item in items.values():
-            if (
-                item_data["name"] == item["name"]
-                and item_data["store_id"] == item["store_id"]
-            ):
-                abort(400, message=f"Item already exists.")
-
-        item_id = uuid.uuid4().hex
-        item = {**item_data, "id": item_id}
-        items[item_id] = item
+    def post(self, item_data):
+        item = ItemModel(**item_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the item.")
 
         return item
