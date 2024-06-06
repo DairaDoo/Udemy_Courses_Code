2a71a2104dcb2c06a35fd1ea9b650545383512bb
DairanDoo <your_email@example.com>
2024-06-06 02:28:30 -0400

    Usando JWT, manejando validaciones con el login, logout y usando blocklist para registrar los tokens de usuarios que hicieron logout y no perimitir su acceso.

--- Curso_RestApi_With_Flask/flask_project01/resources/user.py
+++ Curso_RestApi_With_Flask/flask_project01/resources/user.py
@@ -1,12 +1,16 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt # el access token se envía si el username y password son correctos.
 
 from db import db
+from blocklist import BLOCKLIST
 from models import UserModel
 from schemas import UserSchema
 
 blp = Blueprint("Users", "users", description="Operations on users.")
+
+
 
 @blp.route("/register")
 class UserRegister(MethodView):
@@ -24,6 +28,31 @@ class UserRegister(MethodView):
         db.session.commit()
         
         return {"message": "User created successfully."}, 201    
+    
+    
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+        
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password): # revisamos si el usuario existe y si el password es correcto.
+            access_token = create_access_token(identity=user.id)
+            return {"access_token" : access_token}
+        
+        abort(401, message = "Invalid credentials.")
+        
+    
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out."}
+    
     
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
